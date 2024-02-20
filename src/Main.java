3714a9b76464f14b1b01b682972a2ce524fdd243
Dominik <dominik.severin@protonmail.com>
2024-02-20 15:01:42 +0100

    Testprojekt

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,35 @@
+@SuppressWarnings("unused")
 public class Main {
     public static void main(String[] args) {
-        einlagern("Buch");
+        //einlagern("Buch");
+        rabatt(true, 2000);
     }
+    public static void rabatt(boolean stammkunde, double umsatz){
+        int rabatt;
+        if(stammkunde){
+            if(umsatz > 1000){
+                if(umsatz > 5000){
+                    rabatt = 20;
+                }else{
+                    rabatt = 10;
+                }
+            }else{
+                rabatt = 5;
+            }
+        }else{
+            if(umsatz > 2000){
+                if(umsatz > 10000){
+                    rabatt = 10;
+                }else{
+                    rabatt = 5;
+                }
+            }else{
+                rabatt = 0;
+            }
+        }
+        System.out.println("Rabatt: " + rabatt);
+    }
+
     public static void einlagern(String bezeichnung){
         Lager lager = new Lager();
         int einlagerbar = 1;
